4a0003b2d43a4d7e9595448523fc57d0ce597c50
jesuschavez <jesus.chavez886@myci.csuci.edu>
2018-08-01 19:45:28 -0700

    hi

---
+++ app/src/main/java/com/example/jesuschav/gamert/Forum_Post_Page.java
@@ -0,0 +1,13 @@
+package com.example.jesuschav.gamert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Forum_Post_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum__post__page);
+    }
+}
